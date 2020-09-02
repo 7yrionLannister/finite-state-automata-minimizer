99bcf8f77dfad08ad17a05c591f862340ffff45a
daniel <alejo.dafr@gmail.com>
2020-09-01 20:29:09 -0500

    Added structure for the project

---
+++ test/model/AutomatonTest.java
@@ -0,0 +1,12 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AutomatonTest {
+    @Test
+    public void test() {
+        fail("Not implemented yet");
+    }
+}
