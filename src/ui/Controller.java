cfbe4dd8a693c9169c45c63940f74045c80d9245
Daniel Fernandez <alejo.dafr@gmail.com>
2020-09-27 12:30:09 -0500

    GUI totally integrated with the model

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,13 +1,16 @@
 package ui;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import model.Automaton;
+import model.MealyMachine;
 import model.MooreMachine;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Controller {
 
@@ -20,8 +23,6 @@ public class Controller {
     private ComboBox<String> typeMachine;
     @FXML
     private ScrollPane scrollP1;
-    @FXML
-    private VBox vboxG;
     @FXML
     private ScrollPane scrollP2;
 
@@ -55,31 +56,32 @@ public class Controller {
 
             scrollP1.setContent(gridP1);
             scrollP2.setContent(gridP2);
-            vboxG.getChildren().clear();
-            vboxG.getChildren().add(scrollP1);
 
             try {
                 tf = new TextField[columns + 1][rows];
 
-                for (int i = 1; i < columns + 1; i++) {
-                    TextField ta = new TextField(arrStimulus[i - 1]);
-                    ta.setEditable(false);
-                    ta.setPrefWidth(45);
-                    gridP1.add(ta, i, 0);
-                }
+                fillHeaders(gridP1);
+
                 for (int i = 1; i < rows + 1; i++) {
                     TextField ta = new TextField((char) (i + 64) + "");
                     ta.setEditable(false);
+                    ta.setStyle(ta.getStyle() + "\n-fx-background-color: rgba(125, 156, 205, 0.84)");
                     ta.setPrefWidth(45);
                     gridP1.add(ta, 0, i);
-                    for (int j = 1; j < columns + 2; j++) {
-                        if (j == columns + 1 && typeMachine.getValue().equals("MEALY"))
-                            continue;
+                    for (int j = 1; j < columns + 1; j++) {
                         ta = new TextField("");
                         ta.setPrefWidth(45);
-                        //ta.setStyle("#0000");
+                        ta.setPromptText("A" + (typeMachine.getValue().equals("MEALY") ? ",a" : ""));
                         tf[j - 1][i - 1] = ta;
                         gridP1.add(ta, j, i);
+                    }
+                    if (typeMachine.getValue().equals("MOORE")) {
+                        ta = new TextField("");
+                        ta.setPrefWidth(45);
+                        ta.setStyle(ta.getStyle() + "\n-fx-background-color:  rgba(70, 214, 70, 0.75)");
+                        ta.setPromptText("a");
+                        tf[columns][i - 1] = ta;
+                        gridP1.add(ta, columns + 1, i);
                     }
                 }
             } catch (NegativeArraySizeException | IllegalArgumentException e) {
@@ -95,17 +97,17 @@ public class Controller {
     }
 
     @FXML
-    public void readMachine(ActionEvent event) {
-        readMoore();
+    public void minimizeMachine(Event event) {
+        fillHeaders(gridP2);
+        if(typeMachine.getValue().equals("MOORE")) {
+            readMoore();
+        } else {
+            readMealy();
+        }
     }
 
     public void readMoore() {
-        String[][] matrix = new String[columns + 1][rows];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[i][j] = tf[i][j].getText();
-            }
-        }
+        String[][] matrix = readTextFields("MOORE");
 
         MooreMachine<Character, Character, Character> mooreMachine = new MooreMachine<Character, Character, Character>('A', matrix[matrix.length - 1][0].charAt(0));
 
@@ -123,23 +125,19 @@ public class Controller {
         mooreMachine = mooreMachine.minimize();
         Alert a = new Alert(Alert.AlertType.INFORMATION);
         int minStates = mooreMachine.getOrder();
-        a.setContentText("El automata fue minimizado.\nAhora contiene " + minStates + " estados");
+        a.setContentText("El automata de Moore fue minimizado.\nAhora contiene " + minStates + " estados");
         a.show();
 
-        for (int i = 1; i < columns + 1; i++) {
-            TextField ta = new TextField(arrStimulus[i - 1]);
-            ta.setEditable(false);
-            ta.setPrefWidth(45);
-            gridP2.add(ta, i, 0);
-        }
         ArrayList<Character> minStatesList = mooreMachine.getVertices();
         for (int i = 1; i < minStates + 1; i++) {
             char current = minStatesList.get(i - 1);
             TextField ta = new TextField(current + "");
             ta.setEditable(false);
             ta.setPrefWidth(45);
+            ta.setStyle(ta.getStyle() + "\n-fx-background-color: rgba(125, 156, 205, 0.84)");
             gridP2.add(ta, 0, i);
             ta = new TextField("" + mooreMachine.getResponse(current));
+            ta.setStyle(ta.getStyle() + "\n-fx-background-color:  rgba(70, 214, 70, 0.75)");
             ta.setEditable(false);
             ta.setPrefWidth(45);
             gridP2.add(ta, columns + 1, i);
@@ -152,5 +150,78 @@ public class Controller {
                 gridP2.add(ta, j, i);
             }
         }
+    }
+
+    public void readMealy() {
+        String[][] matrix = readTextFields("MEALY");
+
+        for(int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+
+        MealyMachine<Character, Character, Character> mealyMachine = new MealyMachine<>('A');
+
+        for (int j = 1; j < matrix[0].length; j++) {
+            char temp = (char) (j + 65);
+            mealyMachine.insertState(temp);
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                char temp = (char) (j + 65);
+                String[] cell = matrix[i][j].split(",");
+                mealyMachine.relate(temp, cell[0].charAt(0), arrStimulus[i].charAt(0), cell[1].charAt(0));
+            }
+        }
+
+        mealyMachine = mealyMachine.minimize();
+        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        int minStates = mealyMachine.getOrder();
+        a.setContentText("El automata de Mealy fue minimizado.\nAhora contiene " + minStates + " estados");
+        a.show();
+
+        ArrayList<Character> minStatesList = mealyMachine.getVertices();
+        for (int i = 1; i < minStates + 1; i++) {
+            char current = minStatesList.get(i - 1);
+            TextField ta = new TextField(current + "");
+            ta.setEditable(false);
+            ta.setPrefWidth(45);
+            ta.setStyle(ta.getStyle() + "\n-fx-background-color: rgba(125, 156, 205, 0.84)");
+            gridP2.add(ta, 0, i);
+            for (int j = 1; j < columns + 1; j++) {
+                char stimulus = arrStimulus[j - 1].charAt(0);
+                String cell = "" + mealyMachine.stateTransitionFunction(current, stimulus);
+                cell += "," + mealyMachine.getResponse(current, stimulus);
+                ta = new TextField(cell);
+                ta.setEditable(false);
+                ta.setPrefWidth(45);
+                //ta.setStyle("#0000");
+                tf[j - 1][i - 1] = ta;
+                gridP2.add(ta, j, i);
+            }
+        }
+    }
+
+    private void fillHeaders(GridPane grid) {
+        for (int i = 1; i < columns + 1; i++) {
+            TextField ta = new TextField(arrStimulus[i - 1]);
+            ta.setEditable(false);
+            ta.setStyle(ta.getStyle() + "\n-fx-background-color: rgba(205, 125, 125, 0.84)");
+            ta.setPrefWidth(45);
+            grid.add(ta, i, 0);
+        }
+    }
+
+    /** Reads the input the user put in the matrix taking into account if it was a Moore or a Mealy machine as the number of columns differ
+     * @param type Whether the machine is a Moore machine or a Mealy machine
+     * @return The values of the text field matrix the user gave as input
+     * */
+    private String[][] readTextFields(String type) {
+        String[][] matrix = new String[type.equals("MOORE") ? columns + 1 : columns][rows];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = tf[i][j].getText();
+            }
+        }
+        return matrix;
     }
 }
